3aa29824ef10df4fb3c20a65dfffd74c93c05326
Touhed <mr.touhed.i@outlook.com>
2024-08-04 19:52:28 +0600

    complete deploy

--- src/ProtectedRoute/Protected.jsx
+++ src/ProtectedRoute/Protected.jsx
@@ -6,7 +6,7 @@ import Loading from "../components/Loading";
 // eslint-disable-next-line react/prop-types
 const Protected = ({children}) => {
     const {isLoading,currentUser} = useContextHooks()
-  
+    console.log({isLoading,currentUser});
     if(isLoading) return <Loading/>;
     if(!currentUser) return <Navigate to="/register" replace={true}/> // not registered user redirect to registerd page
 
